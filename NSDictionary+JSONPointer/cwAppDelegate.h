3787e3ea114371ae27de163273a97390655d5a27
Jonathan Dring <email@jonathandring.com>
2014-01-31 14:33:59 +0800

    Initial Commit

---
+++ NSDictionary+JSONPointer/cwAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  cwAppDelegate.h
+//  NSDictionary+JSONPointer
+//
+//  Created by Jonathan on 31/01/2014.
+//  Copyright (c) 2014 Jonathan Dring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cwAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSDictionary+JSONPointer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSDictionary+JSONPointer
+//
+//  Created by Jonathan on 31/01/2014.
+//  Copyright (c) 2014 Jonathan Dring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "cwAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([cwAppDelegate class]));
+    }
+}
